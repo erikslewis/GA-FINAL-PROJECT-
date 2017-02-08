6fd1140a1474412f0be2f3c852bf1142ddaae381
erikslewis <Adelina198527@gmail.com>
2017-02-08 11:35:39 -0500

    auth14

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,9 +69,9 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "127.0.0.1",
-    port: 1025,
-    domain: 'https://dashboard.heroku.com/apps/enigmatic-springs-56858'
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'gmail.com'
     # authentication: "plain",
     # enable_starttls_auto: true,
     # user_name: Rails.application.secrets.email_provider_username,
@@ -79,7 +79,7 @@ Rails.application.configure do
       # domain: Rails.application.secrets.domain_name,
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'https://dashboard.heroku.com/apps/enigmatic-springs-56858' }
+  config.action_mailer.default_url_options = { :host => 'https://enigmatic-springs-56858.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
