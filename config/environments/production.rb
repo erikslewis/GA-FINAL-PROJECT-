75072aa3d1195ab9c2bea65177e5d9fd639312d7
erikslewis <Adelina198527@gmail.com>
2017-02-08 11:41:19 -0500

    auth16

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,18 +71,18 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: 'gmail.com'
-    # authentication: "plain",
-    # enable_starttls_auto: true,
-    # user_name: Rails.application.secrets.email_provider_username,
-    # password: Rails.application.secrets.email_provider_password
-      # domain: Rails.application.secrets.domain_name,
+    domain: 'gmail.com',
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
+
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'https://enigmatic-springs-56858.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
